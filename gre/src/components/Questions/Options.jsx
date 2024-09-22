9cdc597511d70a4a5b1d6b336ededd75d9d79fae
SudevaHarsha <sudevaharsha1@gmail.com>
2024-09-22 14:40:20 +0530

    completed task

--- gre/src/components/Questions/Options.jsx
+++ gre/src/components/Questions/Options.jsx
@@ -58,7 +58,7 @@ const Options = ({ question, selectmode }) => {
                     variant={
                         selectedChoices?.includes(index) ? "default" : "outline"
                     }
-                    className={`justify-start w-full py-8 mb-4 rounded hover:bg-gray-200 ${selectedChoices?.includes(index)
+                    className={`justify-start w-full h-auto py-4 mb-4 rounded hover:bg-gray-200 ${selectedChoices?.includes(index)
                         ? "bg-black text-white hover:bg-black"
                         : "bg-slate-100 text-black"
                         }`}
@@ -69,7 +69,7 @@ const Options = ({ question, selectmode }) => {
                 >
                     <div className="flex items-center justify-center">
                         <div className={`w-7 h-7 mr-5 flex items-center justify-center border ${question.option === 1 ? "rounded-full" : ""} ${selectedChoices?.includes(index) ? "" : "border-black"}`}>{index + 1}</div>
-                        <div className="text-start">{option}</div>
+                        <div className="text-start text-wrap">{option}</div>
                     </div>
 
                 </Button>
@@ -82,7 +82,7 @@ const Options = ({ question, selectmode }) => {
                     variant={
                         selectedChoices.includes(index) ? "default" : "outline"
                     }
-                    className={`justify-between w-full py-8 mb-4 rounded hover:bg-gray-200 ${correctAnswer.includes(index) ? "bg-green-400 text-white hover:bg-green-500 hover:text-white" : selectedChoices.includes(index) && !correctAnswer.includes(index)
+                    className={`justify-between w-full h-auto py-4 mb-4 rounded hover:bg-gray-200 ${correctAnswer.includes(index) ? "bg-green-400 text-white hover:bg-green-500 hover:text-white" : selectedChoices.includes(index) && !correctAnswer.includes(index)
                         ? "bg-red-500 text-white hover:bg-red-600"
                         : "bg-slate-100 text-black"
                         }`}
@@ -93,7 +93,7 @@ const Options = ({ question, selectmode }) => {
                 >
                     <div className="flex items-center justify-start">
                         <div className="p-2 px-3 mr-5 border">{index + 1}</div>
-                        <div className="text-start">{option}</div>
+                        <div className="text-start text-wrap">{option}</div>
                     </div>
                     {(correctAnswer.includes(index) && selectedChoices.includes(index)) && <div>
                         <FaCheckCircle size={32} color="green" />
